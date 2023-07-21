125395b0080c4f0e66b4311b925bfa8cc8de0d52
Sujal Vadgama <65341942+sujalvadgama77@users.noreply.github.com>
2023-07-21 17:52:00 +0530

    app lab - 6

--- SEM-5/APP/GGPlot.R
+++ SEM-5/APP/GGPlot.R
@@ -1,7 +1,9 @@
-ggplot(data = <DATASET>, mapping = aes(<MAPPINGS>)) +
-<GEOM_FUNCTION>()
-ggplot(mpg, aes(x = cty)) + geom_histogram() + labs(x = “city
-                                                      milages”)
-ggplot(mpg, aes(x = cty)) + geom_freqpoly() + labs(x = “city milages”)
-ggplot(mpg, aes(x = cty, y=hwy, color = class)) + geom_point() + labs(x
-                                                                        = “city milages”)
+getwd()
+setwd("D:/22IT608")
+data <- read.csv("stdData.csv")
+
+library(ggplot2)
+data(iris)
+View(iris)
+IrisPlot <- ggplot(data, aes(Name, City, colour=SPI))+geom_point()
+print(IrisPlot)
